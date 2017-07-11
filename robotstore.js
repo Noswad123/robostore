3bc5480bbebc113fc2ff4eb18d354cc84a5ab927
noswad123 <jamal.a.dawson@gmail.com>
2017-07-10 23:53:44 -0500

    update

--- robotstore.js
+++ robotstore.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
-    $("#mega").hide()
-    $("#ret").hide()
-    $("#butler").hide()
+  var robot="";
+  var mega="mega";
+  var ret="ret";
+  var butler="butler";
+  function hideAll()
+  {
+    $("#mega").hide();
+    $("#ret").hide();
+    $("#butler").hide();
+  }
 
-    $(".searchbox").val()
-   // jQuery methods go here...
+  hideAll();
+  $("#searchButton").click(function(){
+
+    var i=0;
+    robot="#"+$(".searchbox").val();
+    i=robot.length;
+    hideAll();
+        if(robot.substring(1,i)==mega.substring(0,i-1))
+        {
+
+          console.log(robot.substring(1,i));
+          console.log(mega.substring(0,i-1));
+        $("#"+mega).show();
+        }
+
+       if(robot.substring(1,i)==ret.substring(0,i-1))
+        {
+
+          console.log(robot.substring(1,i));
+          console.log(ret.substring(0,i-1));
+          $("#"+ret).show();
+        }
+      if (robot.substring(1,i)==butler.substring(0,i-1))
+        {
+        
+          console.log(robot.substring(1,i));
+          console.log(butler.substring(0,i-1));
+          $("#"+butler).show();
+        }
+
+
+    });
+
 
 });
